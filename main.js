4412c494edc07615392563bf1531f252b901c0f6
CodeOn <86895793+Mkinggrafix@users.noreply.github.com>
2022-03-19 18:29:04 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+const body = document.querySelector('#body');
+const btn1 = document.querySelector('.content');
+
+function firstButton(){
+    btn1.textContent = "CUTIEPIE";
+    body.style.backgroundColor = "crimson";   
+}
+
+function secondButton(){
+    btn1.textContent = "HEARTSTOPPER";
+    body.style.backgroundColor = "darkorange";   
+}
+
+function thirdButton(){
+    btn1.textContent = "JEWEL";
+    body.style.backgroundColor = "deepskyblue";
+}
+
+function fourthButton(){
+    btn1.textContent = "LITTLE HEART";
+    body.style.backgroundColor = "purple";
+}
+
+function fifthButton(){
+    btn1.textContent = "DARLING";
+    body.style.backgroundColor = "green";
+}
+
+function sixthButton(){
+    btn1.textContent = "LITTLE MAMA";
+    body.style.backgroundColor = "brown";
+}
+
+function seventhButton(){
+    btn1.textContent = "LOVEY-DOVEY";
+    body.style.backgroundColor = "orangered";
+}
+
+function eighthButton(){
+    btn1.textContent = "PUMPKIN";
+    body.style.backgroundColor = "peru";
+}
+
+function ninthButton(){
+    btn1.textContent = "SUGAR LIPS";
+    body.style.backgroundColor = "royalblue";
+}
+
+function tenthButton(){
+    btn1.textContent = "I LOVE YOU";
+    body.style.backgroundColor = "coral";
+}
